7abbd4bdb3db488820625f3a2558bc4681a1facd
floretn <ivanse99@yandex.ru>
2020-12-24 20:28:28 +0300

    First version of data base

---
+++ investor-workspace.sql
@@ -0,0 +1,55 @@
+﻿begin;
+
+drop schema if exists investor_workspace cascade;
+
+create schema investor_workspace;
+
+CREATE TABLE investor_workspace.t_sector
+( 
+	id_sector		serial,
+	sector_name		character varying (20),
+	PRIMARY KEY (id_sector)
+);
+
+CREATE TABLE investor_workspace.t_company
+( 
+	id_company		serial,
+	company_name		character varying (20) not null,
+	PRIMARY KEY (id_company)
+);
+
+CREATE TABLE investor_workspace.t_stocks
+( 
+	id_stock		serial,
+	id_sector		integer not null,
+	id_company		integer not null,
+	free_floar		double precision not null,
+	price			numeric (12, 2),
+	dividends		double precision not null,
+	s_type			boolean not null,
+	RTS			double precision not null,
+	IMOEX			double precision not null,
+	PRIMARY KEY (id_stock),
+	FOREIGN KEY (id_sector) REFERENCES investor_workspace.t_sector (id_sector),
+	FOREIGN KEY (id_company) REFERENCES investor_workspace.t_company (id_company)
+);
+
+CREATE TABLE investor_workspace.tcl_types_multiplicators
+( 
+	id_type_multiplicator		serial,
+	multiplicator_name		character varying (20) not null,
+	PRIMARY KEY (id_type_multiplicator)
+);
+
+CREATE TABLE investor_workspace.t_multiplicators
+( 
+	id_multiplicator	serial,
+	id_stock		integer not null,
+	id_type_multiplicator	integer not null,
+	m_values		double precision not null,
+
+	PRIMARY KEY (id_stock),
+	FOREIGN KEY (id_stock) REFERENCES investor_workspace.t_stocks (id_stock),
+	FOREIGN KEY (id_type_multiplicator) REFERENCES investor_workspace.tcl_types_multiplicators (id_type_multiplicator)
+);
+end;
---
+++ investors.sql
@@ -0,0 +1,39 @@
+﻿begin;
+
+drop schema if exists "investors" cascade;
+
+create schema "investors";
+
+CREATE TABLE investors.t_people
+( 
+	id_person		serial,
+	last_name		character varying(15) not null,
+	first_name		character varying(15) not null,
+	patronymic		character varying(15),
+	date_of_birth		date,
+	login			character varying(15) not null,
+	passwd			character varying(100) not null,
+	account			money not null,
+	PRIMARY KEY (id_person) 
+);
+
+CREATE TABLE investors.t_case_of_stocks
+( 
+	id_case_of_stocks	serial,
+	id_person		integer not null,
+	case_name		character varying(15) not null,
+	PRIMARY KEY (id_case_of_stocks),
+	FOREIGN KEY (id_person) REFERENCES investors.t_people (id_person)	
+);
+
+CREATE TABLE investors.t_stocks_of_investor
+( 
+	id_stocks_of_investor	serial,
+	id_stock		integer not null,
+	id_case_of_stocks	integer not null,
+	number_of_stocks	integer not null,
+	PRIMARY KEY (id_stocks_of_investor),
+	FOREIGN KEY (id_stock) REFERENCES investor_workspace.t_stocks (id_stock),
+	FOREIGN KEY (id_case_of_stocks) REFERENCES investors.t_case_of_stocks (id_case_of_stocks)	
+);
+end;
